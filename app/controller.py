7c19f0bfff9b17d7b6e95889eda0f2d9e5e4f74f
Michael <admin@fillthegaplearning.co.uk>
2020-10-31 17:43:36 +0000

    Python works, the webpage returns a partial result. At least the partial result is correct.

--- app/controller.py
+++ app/controller.py
@@ -1,14 +1,22 @@
 from flask import render_template, request, redirect
 from app import app
-from app.models.players import players
+from app.models.players import games
 from app.models.player import Player
 
 @app.route('/')
 def index():
-    return render_template('index.html', players=players)
+    return render_template('index.html')
 
-@app.route('/rock/scissors', methods=['POST'])
+@app.route('/rock/scissors', methods=['GET', 'POST'])
 def return_results():
     selection_1 = request.form['selection1']
     selection_2 = request.form['selection2']
-    return render_template('/')
+    for game in games:
+        if  selection_1 == game["selection_1"] and selection_2 == game["selection_2"]:
+            result = game["result"]
+            print(f"Player 1 {result} Player 2.")
+        else:
+            pass
+    return result
+    
+    return render_template('/', games = games[selection_1, result, selection_2])
--- app/models/player.py
+++ app/models/player.py
@@ -1,6 +1,7 @@
 class Player():
 
-    def __init__(self, selection_1, selection_2, result):
+    def __init__(self, game_id, selection_1, selection_2, result):
+        self.game_id = game_id
         self.selection_1 = selection_1
         self.selection_2 = selection_2
         self.result = result
